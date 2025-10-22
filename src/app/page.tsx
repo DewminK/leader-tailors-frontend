fad18cbc7cebd00cd2a0a7376a0c1d8e1a72de5e
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-10-22 23:02:05 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import DesignCard from "@/dashboard/components/design-card";
-import FeedbackCard from "@/dashboard/components/feedback-card";
-import ServiceCard from "@/dashboard/components/service-card";
-import Dashboard from "@/dashboard/page";
+import DesignCard from "@/app/dashboard/components/design-card";
+import FeedbackCard from "@/app/dashboard/components/feedback-card";
+import ServiceCard from "@/app/dashboard/components/service-card";
+import Dashboard from "@/app/dashboard/page";
 import Image from "next/image";
 
 export default function Home() {
